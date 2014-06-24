d3d514599aad7c011d28609ba08125c694071054
teej107 <teej107@gmail.com>
2014-06-24 16:34:58 -0600

    Make sure commiting is working.

--- src/com/dmillerteej/rucksack/RuckSackPlugin.java
+++ src/com/dmillerteej/rucksack/RuckSackPlugin.java
@@ -1,5 +1,7 @@
 package com.dmillerteej.rucksack;
 
+import java.io.File;
+
 import org.bukkit.plugin.java.JavaPlugin;
 
 import com.dmillerteej.recipes.Backpack;
@@ -7,21 +9,30 @@ import com.dmillerteej.recipes.Backpack;
 /**
  * @author teej107
  */
-public class RuckSack extends JavaPlugin
+public class RuckSackPlugin extends JavaPlugin
 {	
 	private Backpack backpack;
-	private RuckSack rucksack;
+	private RuckSackPlugin rucksack;
 	
 	@Override
 	public void onEnable()
 	{
+		createFile(getDataFolder());
 		backpack = new Backpack();
-		rucksack = new RuckSack();
+		rucksack = new RuckSackPlugin();
 	}
 	
 	@Override
 	public void onDisable()
 	{
 		
+	}
+	
+	public void createFile(File file)
+	{
+		if(!file.exists())
+		{
+			file.mkdirs();
+		}
 	}
 }
